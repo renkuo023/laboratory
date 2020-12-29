40842a2989e9f99487155077c93e019e055ec953
renkuo023 <577402346@qq.com>
2020-12-29 23:36:01 +0800

    m

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -13,8 +13,6 @@
       </swiper-item>
     </swiper>
     <YearProgress></YearProgress>
-
-
   </div>
 </template>
 
@@ -26,88 +24,13 @@ export default {
   data () {
     return {
       images: [
-        '//game.gtimg.cn/images/yxzj/act/a20161209story/world-origin.jpg',
-        '//game.gtimg.cn/images/yxzj/act/a20161209story/world-map.jpg',
+        '//game.gtimg.cn/images/yxzj/act/a20161209story/world-power.jpg',
         '//game.gtimg.cn/images/yxzj/act/a20161209story/world-technology.jpg',
-        '//game.gtimg.cn/images/yxzj/act/a20161209story/world-power.jpg'
-      ],
-      openid: '',
-      tableHeader: [
-        {
-          prop: 'datetime',
-          width: 150,
-          label: '日期',
-          color: '#55C355'
-        },
-        {
-          prop: 'sign_in',
-          width: 152,
-          label: '上班时间'
-        },
-        {
-          prop: 'sign_out',
-          width: 152,
-          label: '下班时间'
-        },
-        {
-          prop: 'work_hour',
-          width: 110,
-          label: '工时'
-        },
-        {
-          prop: 'statusText',
-          width: 110,
-          label: '状态'
-        }
-      ],
-      stripe: true,
-      border: true,
-      outBorder: true,
-      row: [
-        {
-          'id': 1,
-          'status': '正常',
-          'datetime': '04-01',
-          'sign_in_time': '09:30:00',
-          'sign_out_time': '18:30:00',
-          'work_hour': 8
-        }, {
-          'id': 2,
-          'status': '迟到',
-          'datetime': '04-02',
-          'sign_in_time': '10:30:00',
-          'sign_out_time': '18:30:00',
-          'work_hour': 7
-        }, {
-          'id': 29,
-          'status': '正常',
-          'datetime': '04-03',
-          'sign_in_time': '09:30:00',
-          'sign_out_time': '18:30:00',
-          'work_hour': 8
-        }, {
-          'id': 318,
-          'status': '休息日',
-          'datetime': '04-04',
-          'sign_in_time': '',
-          'sign_out_time': '',
-          'work_hour': ''
-        }, {
-          'id': 319,
-          'status': '正常',
-          'datetime': '04-05',
-          'sign_in_time': '09:30:00',
-          'sign_out_time': '18:30:00',
-          'work_hour': 8
-        }
-      ],
-      msg: '暂无数据'
+        '//game.gtimg.cn/images/yxzj/act/a20161209story/world-origin.jpg'
+      ]
     }
   },
   methods: {
-    onRowClick: function (e) {
-      console.log('e: ', e)
-    }
   },
   mounted () {
     var that = this
